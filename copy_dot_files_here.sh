886e56f382d72bdf7f35439cf8a6e15b4be56d4c
Joel Joelsson <joel.joelsson@nibe.se>
2025-09-19 12:12:42 +0200

    Add script to copy config files

---
+++ copy_dot_files_here.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+FILES=(
+	"$HOME/.bashrc"
+	"$HOME/.tmux.conf"
+	"$HOME/repos/simemmy-scripts/.luarc.json"
+	"$HOME/repos/simemmy-scripts/.rgignore"
+	)
+
+DEST_DIR=$(pwd)
+
+echo "Backing up config files to $DEST_DIR..."
+
+for FILE in "${FILES[@]}"; do
+	if [ -f "$FILE" ]; then
+		cp "$FILE" "$DEST_DIR"
+		echo "Copied: $FILE"
+	else
+		echo "Skipped (not found): $FILE"
+	fi
+done
